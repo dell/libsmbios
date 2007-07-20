e216dd69f2ecc6675a0ab50991dfc55e291e8f7f
Michael E Brown <michael_e_brown@dell.com>
2007-07-20 18:06:00 -0500

    full path to executables to support split builddir/srcdir builds

--- cppunit/runtests.sh
+++ cppunit/runtests.sh
@@ -6,18 +6,17 @@ set -e
 DIR=$(cd $(dirname $0); pwd)
 TMPDIR=$(mktemp -d  /tmp/unittest-$$-$RANDOM-XXXXXX)
 trap "rm -rf $TMPDIR" EXIT QUIT HUP TERM INT
-cd $DIR
 
 echo -e "\n\nRunning test for RBU"
-./testRbu        $DIR/cppunit $TMPDIR rbu        $DIR/platform/rbu
+cppunit/testRbu        $DIR/cppunit $TMPDIR rbu        $DIR/platform/rbu
 
 echo -e "\n\nRunning Standalone tests."
-./testStandalone $DIR/cppunit $TMPDIR standalone $DIR/platform/opti
+cppunit/testStandalone $DIR/cppunit $TMPDIR standalone $DIR/platform/opti
 
 for i in $DIR/platform/opti ${UNIT_TEST_DATA_DIR}/platform/*; do
     [ -e $i/autotest_flag ] || continue
     echo -e "\n\nRunning test for $i"
-    ./testPlatform   $DIR/cppunit $TMPDIR $(basename $i) $i
+    cppunit/testPlatform   $DIR/cppunit $TMPDIR $(basename $i) $i
     [ $? -eq 0 ] || exit 1
 done
 
