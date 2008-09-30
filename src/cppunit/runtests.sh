0e6dd96f93a642ff083002b86a1dcfc402453453
Michael E Brown <mebrown@michaels-house.net>
2008-09-29 23:09:11 -0500

    move around some of the test .cpp/.h files to get rid of unneeded dir structure

--- src/cppunit/runtests.sh
+++ src/cppunit/runtests.sh
@@ -10,23 +10,22 @@ trap "rm -rf $TMPDIR" EXIT QUIT HUP TERM INT
 TST=out
 
 echo -e "\n\nRunning CInterface tests."
-#$TST/testC_memory_cmos $DIR/cppunit $TMPDIR standalone $DIR/platform/opti
 $TST/testC_memory_cmos $TMPDIR 
 
 echo -e "\n\nRunning test for RBU"
-$TST/testRbu  $TMPDIR  $DIR/platform/rbu
+$TST/testRbu  $TMPDIR  $DIR/test_data/rbu
 
 echo -e "\n\nRunning Standalone tests."
-$TST/testStandalone $TMPDIR $DIR/platform/opti
+$TST/testStandalone $TMPDIR $DIR/test_data/opti
 
-for i in $DIR/platform/opti ${UNIT_TEST_DATA_DIR}/platform/*; do
+for i in $DIR/test_data/opti ${UNIT_TEST_DATA_DIR}/platform/*; do
     [ -e $i/autotest_flag ] || continue
     echo -e "\n\nRunning test for $i"
     $TST/testC_smbios $TMPDIR $(basename $i) $i
     [ $? -eq 0 ] || exit 1
 done
 
-for i in $DIR/platform/opti ${UNIT_TEST_DATA_DIR}/platform/*; do
+for i in $DIR/test_data/opti ${UNIT_TEST_DATA_DIR}/platform/*; do
     [ -e $i/autotest_flag ] || continue
     echo -e "\n\nRunning test for $i"
     $TST/testPlatform $TMPDIR $(basename $i) $i
---
+++ src/include/smbios_c/system_info.h
@@ -0,0 +1,62 @@
+// vim:expandtab:autoindent:tabstop=4:shiftwidth=4:filetype=c:
+/*
+ * Copyright (C) 2005 Dell Inc.
+ *  by Michael Brown <Michael_E_Brown@dell.com>
+ * Licensed under the Open Software License version 2.1 
+ * 
+ * Alternatively, you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ */
+
+
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+// include smbios_c/compat.h first
+#include "smbios_c/compat.h"
+#include "smbios_c/types.h"
+
+#include <stdio.h>  // for size_t
+
+// abi_prefix should be last header included before declarations
+#include "smbios/config/abi_prefix.hpp"
+
+EXTERN_C_BEGIN;
+
+    //////////////////////////////////////////////////////////////////////////
+    // 
+    // Stable API section.
+    // 
+    // All of the functions in this section have a strong guarantee that we 
+    // will not break API compatibility.
+    //
+    //////////////////////////////////////////////////////////////////////////
+
+    //! Return a string representing the version of the libsmbios library.
+    /** Returns the current version of the SMBIOS library as a string
+     */
+    const char *smbios_get_library_version_string(); 
+    int smbios_get_library_version_major(); 
+    int smbios_get_library_version_minor(); 
+
+
+    //! Return the Dell System ID Byte or Word
+    /** The Dell System ID is a unique number allocated to each Dell System
+     * (server, desktop, workstation, or laptop) that uniquely identifies that
+     * system within Dell's product line.
+     */
+    int         smbios_get_dell_system_id(); 
+
+EXTERN_C_END;
+
+// always should be last thing in header file
+#include "smbios/config/abi_suffix.hpp"
+
+#endif  /* SYSTEMINFO_H */
