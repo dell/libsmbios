2f8e5eee262f3f39b235d2e207f3ce567f0323d1
Michael E Brown <michael_e_brown@dell.com>
2008-09-30 16:41:25 -0500

    fixup type for cmos callback fn.

--- src/include/smbios_c/cmos.h
+++ src/include/smbios_c/cmos.h
@@ -40,7 +40,7 @@ int    cmos_write_byte(const struct cmos_obj *, u32 indexPort, u32 dataPort, u32
 size_t cmos_fmt_err(const struct cmos_obj *, char *buf, size_t len);
 
 // useful for checksums, etc
-typedef void (*cmos_write_callback)(struct cmos_obj *, void *);
+typedef void (*cmos_write_callback)(const struct cmos_obj *, void *);
 void register_write_callback(struct cmos_obj *, cmos_write_callback, void *);
 
 EXTERN_C_END;
