d7f6fd5c5a0b261a51f17479432be7dcc42d1301
Michael E Brown <michael_e_brown@dell.com>
2008-10-01 18:09:45 -0500

    begin adding support to pull service tag from cmos.

--- src/libsmbios_c/system_info/service_tag.c
+++ src/libsmbios_c/system_info/service_tag.c
@@ -28,6 +28,54 @@
 #include "dell_magic.h"
 #include "_impl.h"
 
+__internal char *getServiceTagFromCMOSToken()
+{
+    struct smbios_table *table = smbios_factory(SMBIOS_GET_SINGLETON);
+    const struct smbios_struct *s;
+    char *tempval = 0;
+
+    if (!table)
+        goto out;
+
+    s = smbios_get_next_struct_by_type(table, 0, 0xD4);
+    if (!s)
+        goto out;
+
+#if 0
+    // Step 1: Get tag from CMOS
+    tempval = new char[SVC_TAG_LEN_MAX + 1];
+    memset(tempval, '\0', SVC_TAG_LEN_MAX + 1);
+    // will throw an exception if not found.
+    (*table)[Cmos_Service_Token]->getString(reinterpret_cast<u8*>(tempval), SVC_TAG_CMOS_LEN_MAX + 1);
+
+    // Step 2: Decode 7-char tag from 5-char CMOS value
+    dell_decode_service_tag( tempval, SVC_TAG_LEN_MAX + 1 );
+
+    // Step 3: Make sure checksum is good before returning value
+    u16 indexPort, dataPort;
+    u8  location;
+
+    smbios::IToken *token = &(*((*table)[ Cmos_Service_Token ]));
+    dynamic_cast< smbios::ICmosToken * >(token)->getCMOSDetails( &indexPort, &dataPort, &location );
+
+    u8 csum = 0;
+    ICmosRW *cmos = cmos::CmosRWFactory::getFactory()->getSingleton();
+
+    for( u32 i = 0; i < SVC_TAG_CMOS_LEN_MAX; i++)
+    {
+        // stupid stuff to avoid MVC++ .NET runtime exception check for cast to different size
+        csum = (csum + cmos->readByte( indexPort, dataPort, location + i )) & 0xFF;
+    }
+
+    // stupid stuff to avoid MVC++ .NET runtime exception check for cast to different size
+    csum = (csum - cmos->readByte( indexPort, dataPort, location + SVC_TAG_CMOS_LEN_MAX )) & 0xFF;
+    if( csum ) // bad (should be zero)
+        throw "Bad checksum";
+#endif
+
+out:
+    return tempval;
+}
 
 __internal char *getServiceTagFromSysInfo()
 {
@@ -48,7 +96,7 @@ struct DellGetServiceTagFunctions
 /* try dynamic functions first to make sure we get current data. */
 DellGetServiceTagFunctions[] = {
                                    //{&getServiceTagFromSMI,},       // SMI Token
-                                   //{&getServiceTagFromCMOSToken,}, // CMOS Token
+                                   {&getServiceTagFromCMOSToken,}, // CMOS Token
                                    {&getServiceTagFromSysInfo,},   // SMBIOS System Information Item
                                    {&getServiceTagFromSysEncl,},   // SMBIOS System Enclosure Item
                                };
@@ -67,9 +115,3 @@ char *smbios_get_service_tag()
     strip_trailing_whitespace(serviceTag);
     return serviceTag;
 }
-
-
-
-
-
-
