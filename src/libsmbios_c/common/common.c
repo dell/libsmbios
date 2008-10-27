5d34716f59d87b6a8ad1d6082fdd06ee7065a785
Michael E Brown <mebrown@michaels-house.net>
2008-10-27 02:37:57 -0500

    add proper fnprintf() debugging to constructor.

--- src/libsmbios_c/common/common.c
+++ src/libsmbios_c/common/common.c
@@ -2,10 +2,18 @@
 #include "config.h"
 #endif
 
+#define LIBSMBIOS_C_SOURCE
 #include "libsmbios_c_intlize.h"
+
+#if defined(DEBUG_CONSTRUCTOR_C)
+#   include <stdio.h>
+#   undef dbg_printf
+#   define dbg_printf _dbg_printf
+#endif
 
 __attribute__((constructor)) static void lib_initialize (void)
 {
+    fnprintf("CONSTRUCTOR: pkg: %s, dir: %s\n", GETTEXT_PACKAGE,LIBSMBIOS_LOCALEDIR);
     bindtextdomain (GETTEXT_PACKAGE, LIBSMBIOS_LOCALEDIR);
 }
 
