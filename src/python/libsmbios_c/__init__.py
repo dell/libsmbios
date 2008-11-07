7ea77f726c3182177944d409bd7deb1058643a49
Michael E Brown <michael_e_brown@dell.com>
2008-11-07 01:14:47 -0600

    add __init__ code to pull all the modules into libsmbios_c

---
+++ src/python/libsmbios_c/__init__.py
@@ -0,0 +1,14 @@
+import cmos
+import memory
+import smbios
+import smi
+import system_info
+import token
+from _vars import *
+
+__VERSION__ = RELEASE_VERSION
+
+_all_ = [
+    "cmos", "memory", "smbios", "smi", "system_info", "token",
+    "localedir", "pkgdatadir", "pythondir", "pkgconfdir"
+    ]
