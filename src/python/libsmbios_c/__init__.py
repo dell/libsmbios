88e72f6eca4c66ae93dff74e60a476a8fd9d5f90
Michael E Brown <mebrown@michaels-house.net>
2008-11-09 00:45:54 -0600

    let python utils work when called from build tree without setting LD_LIBRARY_PATH if top_srcdir == top_builddir

--- src/python/libsmbios_c/__init__.py
+++ src/python/libsmbios_c/__init__.py
@@ -4,21 +4,22 @@
 # LD_LIBRARY_PATH manually for that)
 import os
 from _vars import *
-":".join(os.environ.get("LD_LIBRARY_PATH", "").split(":") + [libdir,])
-
-import gettext
-t = gettext.translation(GETTEXT_PACKAGE, localedir, fallback=True)
-_ = t.ugettext
 
 # import the .so one time rather than in each submodule
 import ctypes
-libsmbios_c_DLL = ctypes.cdll.LoadLibrary("libsmbios_c.so.2")
+try:
+    # try explicit path first
+    # this lets us run cleanly from build tree
+    # or if user uses wierd --prefix for configure
+    # doesnt work from build tree if top_builddir != top_srcdir
+    libsmbios_c_DLL = ctypes.cdll.LoadLibrary(os.path.join(libdir, "libsmbios_c.so.2"))
+except OSError, e:
+    # then let it search
+    libsmbios_c_DLL = ctypes.cdll.LoadLibrary("libsmbios_c.so.2")
 
 # check ctypes bool support. If it doesnt exist, fake it.
 if not getattr(ctypes, "c_bool", None):
     ctypes.c_bool = ctypes.c_uint8
-
-import _common
 
 # import all our packages.
 import cmos
@@ -29,8 +30,10 @@ import system_info
 import token
 
 # push gettext vars into namespaces
+import gettext
+t = gettext.translation(GETTEXT_PACKAGE, localedir, fallback=True)
 for i in (cmos, memory, smbios, smi, system_info, token, _common):
-    setattr(i, "_", _)
+    setattr(i, "_", t.ugettext)
 
 __VERSION__ = RELEASE_VERSION
 
