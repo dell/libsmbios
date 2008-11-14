b76290c602fcd2e4f9078d9dfe809f24b4bcaa4e
Michael E Brown <michael_e_brown@dell.com>
2008-11-14 00:14:52 -0600

    consolidate _TokenTable and _token_table. Dont decorate __del__ since it is called without a stack frame in some cases.

--- src/python/libsmbios_c/token.py
+++ src/python/libsmbios_c/token.py
@@ -26,6 +26,9 @@ TOKEN_GET_SINGLETON =0x0001
 TOKEN_GET_NEW       =0x0002
 TOKEN_UNIT_TEST_MODE=0x0004
 
+class TokenTableParseError(Exception): pass
+class TokenManipulationFailure(Exception): pass
+
 class Token(ctypes.Structure): 
     decorate(traceLog())
     def getId(self):
@@ -93,16 +96,16 @@ def TokenTable(flags=TOKEN_GET_SINGLETON, factory_args=None):
         _TokenTable._instance = _TokenTable( flags, *factory_args)
     return _TokenTable._instance
 
-class _TokenTable(object):
+class _TokenTable(ctypes.Structure):
     _instance = None
     decorate(traceLog())
     def __init__(self, *args):
         self._tableobj = None
         self._tableobj = DLL.token_table_factory(*args)
 
-    decorate(traceLog())
     def __del__(self):
-        DLL.token_table_free(self._tableobj)
+        if self._tableobj is not None:
+            DLL.token_table_free(self._tableobj)
 
     decorate(traceLog())
     def __iter__(self):
@@ -126,17 +129,14 @@ class _TokenTable(object):
             raise exceptions.IndexError( _("no such ID") )
 
 
-#struct token_table;
-class _token_table(ctypes.Structure): pass
-
 #// format error string
 #const char *token_table_strerror(const struct token_table *m);
 # define strerror first so we can use it in error checking other functions.
-DLL.token_table_strerror.argtypes = [ ctypes.POINTER(_token_table) ]
+DLL.token_table_strerror.argtypes = [ ctypes.POINTER(_TokenTable) ]
 DLL.token_table_strerror.restype = ctypes.c_char_p
 decorate(traceLog())
 def _table_strerror(obj):
-    return Exception(DLL.token_table_strerror(obj))
+    return DLL.token_table_strerror(obj)
 
 #const char *token_obj_strerror(const struct token_table *m);
 # define strerror first so we can use it in error checking other functions.
@@ -144,24 +144,24 @@ DLL.token_obj_strerror.argtypes = [ ctypes.POINTER(Token) ]
 DLL.token_obj_strerror.restype = ctypes.c_char_p
 decorate(traceLog())
 def _obj_strerror(obj):
-    return Exception(DLL.token_obj_strerror(obj))
+    return DLL.token_obj_strerror(obj)
 
 #struct token_table *token_table_factory(int flags, ...);
 # dont define argtypes because this is a varargs function...
 #DLL.token_table_factory.argtypes = [ctypes.c_int, ]
-DLL.token_table_factory.restype = ctypes.POINTER(_token_table)
-DLL.token_table_factory.errcheck = errorOnNullPtrFN(lambda r,f,a: _table_strerror(r))
+DLL.token_table_factory.restype = ctypes.POINTER(_TokenTable)
+DLL.token_table_factory.errcheck = errorOnNullPtrFN(lambda r,f,a: TokenTableParseError(_table_strerror(r)))
 
 #void   token_table_free(struct token_table *);
-DLL.token_table_free.argtypes = [ ctypes.POINTER(_token_table) ]
+DLL.token_table_free.argtypes = [ ctypes.POINTER(_TokenTable) ]
 DLL.token_table_free.restype = None
 
 #const struct token_obj * DLL_SPEC token_table_get_next(const struct token_table *, const struct token_obj *cur);
-DLL.token_table_get_next.argtypes = [ ctypes.POINTER(_token_table), ctypes.POINTER(Token) ]
+DLL.token_table_get_next.argtypes = [ ctypes.POINTER(_TokenTable), ctypes.POINTER(Token) ]
 DLL.token_table_get_next.restype = ctypes.POINTER(Token)
 
 #const struct token_obj * DLL_SPEC token_table_get_next_by_id(const struct token_table *, const struct token_obj *cur, u16 id);
-DLL.token_table_get_next_by_id.argtypes = [ ctypes.POINTER(_token_table), ctypes.POINTER(Token), ctypes.c_uint16 ]
+DLL.token_table_get_next_by_id.argtypes = [ ctypes.POINTER(_TokenTable), ctypes.POINTER(Token), ctypes.c_uint16 ]
 DLL.token_table_get_next_by_id.restype = ctypes.POINTER(Token)
 
 #u16  DLL_SPEC token_obj_get_id(const struct token_obj *);
@@ -179,12 +179,12 @@ DLL.token_obj_is_bool.restype = ctypes.c_bool
 #bool  DLL_SPEC token_obj_is_active(const struct token_obj *);
 DLL.token_obj_is_active.argtypes = [ ctypes.POINTER(Token) ]
 DLL.token_obj_is_active.restype = ctypes.c_int
-DLL.token_obj_is_active.errcheck = errorOnNegativeFN(lambda r,f,a: _table_strerror(a[0]))
+DLL.token_obj_is_active.errcheck = errorOnNegativeFN(lambda r,f,a: TokenManipulationFailure(_table_strerror(a[0])))
 
 #int  DLL_SPEC token_obj_activate(const struct token_obj *);
 DLL.token_obj_activate.argtypes = [ ctypes.POINTER(Token) ]
 DLL.token_obj_activate.restype = ctypes.c_int
-DLL.token_obj_activate.errcheck = errorOnNegativeFN(lambda r,f,a: _table_strerror(a[0]))
+DLL.token_obj_activate.errcheck = errorOnNegativeFN(lambda r,f,a: TokenManipulationFailure(_table_strerror(a[0])))
 
 #bool  DLL_SPEC token_obj_is_string(const struct token_obj *);
 DLL.token_obj_is_string.argtypes = [ ctypes.POINTER(Token) ]
@@ -193,12 +193,12 @@ DLL.token_obj_is_string.restype = ctypes.c_bool
 #char*  DLL_SPEC token_obj_get_string(const struct token_obj *, size_t *len);
 DLL.token_obj_get_string.argtypes = [ ctypes.POINTER(Token), ctypes.POINTER(ctypes.c_size_t) ]
 DLL.token_obj_get_string.restype = ctypes.c_void_p
-DLL.token_obj_get_string.errcheck = errorOnNullPtrFN(lambda r,f,a: _table_strerror(a[0]))
+DLL.token_obj_get_string.errcheck = errorOnNullPtrFN(lambda r,f,a: TokenManipulationFailure(_table_strerror(a[0])))
 
 #int  DLL_SPEC token_obj_set_string(const struct token_obj *, const char *, size_t size);
 DLL.token_obj_set_string.argtypes = [ ctypes.POINTER(Token), ctypes.c_char_p, ctypes.c_size_t ]
 DLL.token_obj_set_string.restype = ctypes.c_int
-DLL.token_obj_set_string.errcheck = errorOnNegativeFN(lambda r,f,a: _table_strerror(a[0]))
+DLL.token_obj_set_string.errcheck = errorOnNegativeFN(lambda r,f,a: TokenManipulationFailure(_table_strerror(a[0])))
 
 #const struct smbios_struct * DLL_SPEC token_obj_get_smbios_struct(const struct token_obj *);
 #DLL.token_obj_get_smbios_struct.argtypes = [ ctypes.POINTER(Token), ]
@@ -211,7 +211,7 @@ DLL.token_obj_try_password.restype = ctypes.c_int
 #const void * DLL_SPEC token_obj_get_ptr(const struct token_obj *t);
 DLL.token_obj_get_ptr.argtypes = [ ctypes.POINTER(Token), ]
 DLL.token_obj_get_ptr.restype = ctypes.POINTER(_Token_LL)
-DLL.token_obj_get_ptr.errcheck = errorOnNullPtrFN(lambda r,f,a: _table_strerror(r))
+DLL.token_obj_get_ptr.errcheck = errorOnNullPtrFN(lambda r,f,a: TokenManipulationFailure(_table_strerror(r)))
 
 
 
