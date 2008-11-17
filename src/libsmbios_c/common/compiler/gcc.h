2e21c86082d9e6c969f806f934158909bdedd562
Michael E Brown <michael_e_brown@dell.com>
2008-11-17 12:11:15 -0600

    flush buffers after each debugging print.

--- src/libsmbios_c/common/compiler/gcc.h
+++ src/libsmbios_c/common/compiler/gcc.h
@@ -2,7 +2,7 @@
 #define __internal __attribute__((visibility("internal")))
 #define __hidden __attribute__((visibility("hidden")))
 #define _null_call( args...) do {} while(0)
-#define _dbg_printf(format, args...) do { fprintf(stderr , format , ## args);  } while(0)
+#define _dbg_printf(format, args...) do { fprintf(stderr , format , ## args); fflush(NULL); } while(0)
 // dbg_printf gets redefined to _dbg_printf on a source file by source file basis
 
 #ifdef DEBUG_OUTPUT_ALL
