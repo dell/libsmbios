29fca75c843f730ad8e86bbd2be52754370d5b85
Michael E Brown <mebrown@michaels-house.net>
2008-11-20 00:47:21 -0600

    remove c++ from c headers

--- src/libsmbios_c/common/platform/win64.h
+++ src/libsmbios_c/common/platform/win64.h
@@ -1,6 +1,6 @@
 // Enable 64-bit file access (changes off_t to 64-bit)
 #ifndef FSEEK
-#define FSEEK(fh, pos, whence) fseek(fh, static_cast<long>(pos), whence)
+#define FSEEK(fh, pos, whence) fseek(fh, (long)(pos), whence)
 #endif
 
 /* Windows is lame. */
