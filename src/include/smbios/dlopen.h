8a69a376351552711059518ba05012f3fe1f0e7b
Michael E Brown <michael_e_brown@dell.com>
2009-01-19 11:39:30 -0600

    add dlopen.h header so people doing dlopen() always dlopen the ver they were compiled against.

---
+++ src/include/smbios/dlopen.h
@@ -0,0 +1,33 @@
+// vim:expandtab:autoindent:tabstop=4:shiftwidth=4:filetype=c:
+/*
+ * Copyright (C) 2005 Dell Inc.
+ *  by Michael Brown <Michael_E_Brown@dell.com>
+ * Licensed under the Open Software License version 2.1
+ *
+ * Alternatively, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+
+
+#ifndef DLOPEN_H
+#define DLOPEN_H
+
+// include smbios_c/compat.h first
+#include "smbios_c/compat.h"
+
+EXTERN_C_BEGIN;
+
+// Use this header if you are using dlopen to ensure that you get the same ABI
+// version as what you are compiling against
+#define LIBSMBIOS_LIBNAME "libsmbios.so.2"
+
+EXTERN_C_END;
+
+#endif  /* DLOPEN_H */
