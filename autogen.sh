75cdf8018af4f6f98d20cd6d12aeff61fc3112ee
Michael E Brown <michael_e_brown@dell.com>
2009-01-19 14:28:28 -0600

    simplify autogen and include built-headers in -devel rpm. Remove autotools-generated text files.

--- autogen.sh
+++ autogen.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
 
-# run this script to create all the autotools fluff
+# run this script to create all the autotools fluff. It will also run configure
+# unless told not to
 
 set -e
 
+CURDIR=$(pwd)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 cd $SCRIPT_DIR
 
-ACLOCALOPTS=
-if grep -q ^AM_GNU_GETTEXT configure.ac 2>/dev/null; then
-    cp configure.ac configure.ac~
-    cp Makefile.am  Makefile.am~
-    autopoint --force # gettextize replacement
-    mv configure.ac~ configure.ac
-    mv Makefile.am~  Makefile.am
-    ACLOCALOPTS="$ACLOCALOPTS -I m4"
+autoreconf -i -f -Wno-portability
+
+run_configure=true
+for arg in $*; do
+    case $arg in
+        --no-configure)
+            run_configure=false
+            ;;
+        *)
+            ;;
+    esac
+done
+
+if test $run_configure = true; then
+    cd $CURDIR
+    $SCRIPT_DIR/configure "$@"
 fi
 
-aclocal $ACLOCALOPTS
-
-if grep -q ^AC_PROG_LIBTOOL configure.ac 2>/dev/null; then
-    libtoolize -c --force --automake
-fi
-
-if grep -q ^AC_CONFIG_HEADER configure.ac 2>/dev/null; then
-    autoheader --force
-fi
-
-automake --force --foreign --add-missing -c
-autoconf --force
