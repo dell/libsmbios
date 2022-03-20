a7d3b8d1cca35c8d4aa806e0aaa019ebd2381348
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 09:47:34 +0000

    Delete src directory

--- src/libsmbios_c/common/platform/win32.h
+++
@@ -1,8 +0,0 @@
-// Enable 64-bit file access (changes off_t to 64-bit)
-#ifndef FSEEK
-#define FSEEK(fh, pos, whence) fseek(fh, (long)(pos), whence)
-#endif
-
-/* Windows is lame. */
-#   define _snprintf    snprintf
-
