d1474dddcf7bfbf6c7ee1254be59b414dec3ec83
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 17:40:17 +0800

    Delete pkg directory

--- pkg/build-dockerfile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cur_dir=$(cd $(dirname $0); pwd)
-docker build -f $cur_dir/dockerfiles/Dockerfile.${1} -t libsmbios-${1} -- .
--- pkg/dockerfiles/centos8-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./pkg/mk-rel-rpm.sh
-cp $BUILD_DIR/_builddir/*.rpm $OUTPUT_DIR
-cp $BUILD_DIR/_builddir/x86_64/*.rpm $OUTPUT_DIR
